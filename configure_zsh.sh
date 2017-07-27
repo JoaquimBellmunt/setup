906b0d26b6f8ba14f55326e921afe584cfa9f239
Joaquim Bellmunt Montoya <joaquim.bell@gmail.com>
2017-07-27 18:26:34 +0800

    Update configure_zsh.sh

--- configure_zsh.sh
+++ configure_zsh.sh
@@ -9,12 +9,3 @@ done
 
 # start zsh ssh agent
 exec ssh-agent zsh
-
-# config for efficient git
-git config --global user.name "Joaquim Bellmunt"
-git config --global user.email "joaquim.bell@gmail.com  "
-ssh-keygen -t rsa -N "" -C "joaquim.bell@gmail.com" -f ~/.ssh/id_rsa
-ssh-add .ssh/id_rsa
-echo "You should copy the next line into a new ssh key on github  https://github.com/settings/ssh)."
-cat ~/.ssh/id_rsa.pub
-echo "Then you can run 'ssh -T git@github.com' to check that the connection is working."
